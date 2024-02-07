6c98cffb6f7d5837b7b24b7eeda82e908a3447b1
chababbb <142672673+chababbb@users.noreply.github.com>
2024-02-07 22:48:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-function compareResult () {
-    const divElem = document.querySelector ('.result');
-    const rText = document.createElement ('div');
-    rText.innerHTML = 'Результаты сравнения';
-    divElem.append (rText);
-    document.body.appendChild (rText);
+//const fromText = document.querySelectorAll('.wordInsert'),
+//translate = document.querySelector('button');
+
+let button = document.querySelector('button');
+//let fromtext = document.querySelector('.wordInsert').value;
+document.querySelector('button').onclick = write;
+//fromtext.addEventListener ('input', write());
+
+//if(document.querySelector(".wordInsert").value==="") { 
+    //button.disabled = true; 
+//} else { 
+   // button.disabled = false;
+//}
+
+function write() {
+    let fromtext = document.querySelector('.wordInsert').value;
+    document.querySelector('.late').innerHTML = fromtext;
+    const targetLanguages = ['be', 'bs', 'bg', 'cs', 'hr', 'mk', 'pl', 'sk', 'sl', 'sr', 'uk'];
+
+    let totext = document.querySelector('.late').value;
+
+    targetLanguages.forEach(targetLanguage => {
+        let apiURL = `https://api.mymemory.translated.net/get?q=${fromtext}!&langpair=ru|${targetLanguage}`;
+        fetch(apiURL).then(res => res.json()).then(data => {
+            const translate = data.responseData.translatedText + targetLanguage;
+            console.log (translate);
+            document.querySelector('.late').innerHTML = translate;
+        });
+    });
+
+    // function showDiv (){}
+    if (document.getElementById('result').style.display != 'inline-flex') {
+        document.getElementById('result').style.display = 'inline-flex';
+    }
+    return true;
 }
